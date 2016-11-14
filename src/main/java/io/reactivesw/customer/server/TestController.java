209fc6b5c5cde812e21e6e552c3c822abb9e2b37
kaka89 <liuquan89@gmail.com>
2016-11-14 14:13:05 +0800

    add api comment

--- src/main/java/io/reactivesw/customer/server/TestController.java
+++ src/main/java/io/reactivesw/customer/server/TestController.java
@@ -1,7 +1,13 @@
 package io.reactivesw.customer.server;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by bruceliu on 16/11/14.
@@ -10,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
+  @ApiOperation(value = "Get test's message")
   @GetMapping("/test")
-  public String testApi(){
-    return "this is the test message";
+  public String testApi(@RequestParam("userId") @ApiParam(value = "User ID", required = true) @NotNull String userId){
+    return "this is the test message, user id is: " + userId;
   }
 }
