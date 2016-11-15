60c197a1e16c06aef32eb89834e5fa03c215ab77
umasuo <umasuo@umasuo.local>
2016-11-15 15:35:56 +0800

    add router

--- src/main/java/io/reactivesw/customer/server/routes/Router.java
+++ src/main/java/io/reactivesw/customer/server/routes/Router.java
@@ -4,5 +4,8 @@ package io.reactivesw.customer.server.routes;
  * Created by bruceliu on 16/11/14.
  */
 public class Router {
-  public static final String TEST_API = "/test";
+    public static final String TEST_API = "/test";
+    public static final String CUSTOMER_ID = "customerId";
+    // for customer apis
+    public static final String CUSTOMER_GET_BY_ID = "/customers/{" + CUSTOMER_ID + "}";
 }
