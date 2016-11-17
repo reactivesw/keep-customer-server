af76037f7d0b2bf5f7f2e6817d7dddb94960bc42
umasuo <umasuo@Bruce-Liu.local>
2016-11-17 17:31:11 +0800

    return item draft

---
+++ src/main/java/io/reactivesw/customer/server/cart/models/draft/ReturnItemDraft.java
@@ -0,0 +1,26 @@
+package io.reactivesw.customer.server.cart.models.draft;
+
+import io.reactivesw.customer.server.cart.enums.ReturnShipmentState;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * The ReturnItemDraft needs to be given with the Add ReturnInfo update method. At this point only
+ * Advised or Returned ReturnShipmentState is allowed. Item with Advised shipment state gets the
+ * NonRefundable ReturnPaymentState and item with Returned shipment state gets Initial payment
+ * state. Created by umasuo on 16/11/17.
+ */
+public class ReturnItemDraft {
+
+  @ApiModelProperty(required = true)
+  private Long quantity;
+
+  @ApiModelProperty(required = true)
+  private String lineItemId;
+
+  @ApiModelProperty(required = false)
+  private String comment;
+
+  @ApiModelProperty(required = true)
+  private ReturnShipmentState shipmentState;
+
+}
