d9ca6642ba2e4cbebeb5b875a46573012f45ab8a
Davis <davis.dai@go6d.com>
2016-11-17 10:50:33 +0800

    edit category model and move customerfields

--- src/main/java/io/reactivesw/customer/server/catalog/models/Category.java
+++ src/main/java/io/reactivesw/customer/server/catalog/models/Category.java
@@ -1,9 +1,13 @@
 package io.reactivesw.customer.server.catalog.models;
 
+import io.reactivesw.customer.server.common.types.CustomFields;
+import io.reactivesw.customer.server.common.types.Reference;
 import io.reactivesw.customer.server.common.utils.JpaConverterJson;
 import io.reactivesw.customer.server.common.types.LocalizedString;
 
 import javax.persistence.Convert;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 /**
  * category model.
@@ -11,27 +15,81 @@ import javax.persistence.Convert;
  */
 public class Category {
   /**
-   * The Id.
+   *  The unique ID of the category.
    */
   private String id;
+
+  /**
+   *  The current version of the category.
+   */
+  private Integer version;
+
+  /**
+   * create time.
+   */
+  private ZonedDateTime createdAt;
+
+  /**
+   * last modified time.
+   */
+  private ZonedDateTime lastModifiedAt;
+
   /**
    * The Name.
    */
-  private String name;
+  private LocalizedString name;
+
+  /**
+   * human-readable identifiers usually used as deep-link URL to the related category.
+   * Each slug is unique across a project,
+   * but a category can have the same slug for different languages.
+   */
+  private LocalizedString slug;
+
   /**
    * The Description.
    */
-  private String description;
+  private LocalizedString description;
+
   /**
-   * The Parent id.
+   *  Contains the parent path towards the root category.
    */
-  private String parentId;
+  private List<Reference> ancestors;
+
+  /**
+   *  A category that is the parent of this category in the category tree.
+   */
+  private Reference parent;
+
+  /**
+   *  An attribute as base for a custom category order in one level.
+   */
+  private String orderHint;
+
+  /**
+   * externalId.
+   */
+  private String externalId;
 
   /**
    * The Meta title.
    */
-  @Convert(converter = JpaConverterJson.class)
   private LocalizedString metaTitle;
+
+  /**
+   * The metaDescription.
+   */
+  private LocalizedString metaDescription;
+
+  /**
+   * the metaKeywords.
+   */
+  private LocalizedString metaKeywords;
+
+  /**
+   * custom fields.
+   */
+  private CustomFields custom;
 
   /**
    * Gets id.
@@ -52,11 +110,65 @@ public class Category {
   }
 
   /**
+   * Gets version.
+   *
+   * @return the version
+   */
+  public Integer getVersion() {
+    return version;
+  }
+
+  /**
+   * Sets version.
+   *
+   * @param version the version
+   */
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  /**
+   * Gets created at.
+   *
+   * @return the created at
+   */
+  public ZonedDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  /**
+   * Sets created at.
+   *
+   * @param createdAt the created at
+   */
+  public void setCreatedAt(ZonedDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  /**
+   * Gets last modified at.
+   *
+   * @return the last modified at
+   */
+  public ZonedDateTime getLastModifiedAt() {
+    return lastModifiedAt;
+  }
+
+  /**
+   * Sets last modified at.
+   *
+   * @param lastModifiedAt the last modified at
+   */
+  public void setLastModifiedAt(ZonedDateTime lastModifiedAt) {
+    this.lastModifiedAt = lastModifiedAt;
+  }
+
+  /**
    * Gets name.
    *
    * @return the name
    */
-  public String getName() {
+  public LocalizedString getName() {
     return name;
   }
 
@@ -65,8 +177,26 @@ public class Category {
    *
    * @param name the name
    */
-  public void setName(String name) {
+  public void setName(LocalizedString name) {
     this.name = name;
+  }
+
+  /**
+   * Gets slug.
+   *
+   * @return the slug
+   */
+  public LocalizedString getSlug() {
+    return slug;
+  }
+
+  /**
+   * Sets slug.
+   *
+   * @param slug the slug
+   */
+  public void setSlug(LocalizedString slug) {
+    this.slug = slug;
   }
 
   /**
@@ -74,7 +204,7 @@ public class Category {
    *
    * @return the description
    */
-  public String getDescription() {
+  public LocalizedString getDescription() {
     return description;
   }
 
@@ -83,26 +213,80 @@ public class Category {
    *
    * @param description the description
    */
-  public void setDescription(String description) {
+  public void setDescription(LocalizedString description) {
     this.description = description;
   }
 
   /**
-   * Gets parent id.
+   * Gets ancestors.
    *
-   * @return the parent id
+   * @return the ancestors
    */
-  public String getParentId() {
-    return parentId;
+  public List<Reference> getAncestors() {
+    return ancestors;
   }
 
   /**
-   * Sets parent id.
+   * Sets ancestors.
    *
-   * @param parentId the parent id
+   * @param ancestors the ancestors
    */
-  public void setParentId(String parentId) {
-    this.parentId = parentId;
+  public void setAncestors(List<Reference> ancestors) {
+    this.ancestors = ancestors;
+  }
+
+  /**
+   * Gets parent.
+   *
+   * @return the parent
+   */
+  public Reference getParent() {
+    return parent;
+  }
+
+  /**
+   * Sets parent.
+   *
+   * @param parent the parent
+   */
+  public void setParent(Reference parent) {
+    this.parent = parent;
+  }
+
+  /**
+   * Gets order hint.
+   *
+   * @return the order hint
+   */
+  public String getOrderHint() {
+    return orderHint;
+  }
+
+  /**
+   * Sets order hint.
+   *
+   * @param orderHint the order hint
+   */
+  public void setOrderHint(String orderHint) {
+    this.orderHint = orderHint;
+  }
+
+  /**
+   * Gets external id.
+   *
+   * @return the external id
+   */
+  public String getExternalId() {
+    return externalId;
+  }
+
+  /**
+   * Sets external id.
+   *
+   * @param externalId the external id
+   */
+  public void setExternalId(String externalId) {
+    this.externalId = externalId;
   }
 
   /**
@@ -124,12 +308,81 @@ public class Category {
   }
 
   /**
+   * Gets meta description.
+   *
+   * @return the meta description
+   */
+  public LocalizedString getMetaDescription() {
+    return metaDescription;
+  }
+
+  /**
+   * Sets meta description.
+   *
+   * @param metaDescription the meta description
+   */
+  public void setMetaDescription(LocalizedString metaDescription) {
+    this.metaDescription = metaDescription;
+  }
+
+  /**
+   * Gets meta keywords.
+   *
+   * @return the meta keywords
+   */
+  public LocalizedString getMetaKeywords() {
+    return metaKeywords;
+  }
+
+  /**
+   * Sets meta keywords.
+   *
+   * @param metaKeywords the meta keywords
+   */
+  public void setMetaKeywords(LocalizedString metaKeywords) {
+    this.metaKeywords = metaKeywords;
+  }
+
+  /**
+   * Gets custom.
+   *
+   * @return the custom
+   */
+  public CustomFields getCustom() {
+    return custom;
+  }
+
+  /**
+   * Sets custom.
+   *
+   * @param custom the custom
+   */
+  public void setCustom(CustomFields custom) {
+    this.custom = custom;
+  }
+
+  /**
    * toString method.
    * @return String
    */
   @Override
   public String toString() {
-    return "Category{" + "id=" + id + ", name='" + name + ", description='" + description + '\''
-        + ", parentId=" + parentId + '}';
+    return "Category{" +
+        "id='" + id + '\'' +
+        ", version=" + version +
+        ", createdAt=" + createdAt +
+        ", lastModifiedAt=" + lastModifiedAt +
+        ", name=" + name +
+        ", slug=" + slug +
+        ", description=" + description +
+        ", ancestors=" + ancestors +
+        ", parent=" + parent +
+        ", orderHint='" + orderHint + '\'' +
+        ", externalId='" + externalId + '\'' +
+        ", metaTitle=" + metaTitle +
+        ", metaDescription=" + metaDescription +
+        ", metaKeywords=" + metaKeywords +
+        ", custom=" + custom +
+        '}';
   }
 }
