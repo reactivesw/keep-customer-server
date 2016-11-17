cc939f0c3165d989533d811017dfb6c797d15ef6
Davis <davis.dai@go6d.com>
2016-11-17 14:46:52 +0800

    edit TaxRate

--- src/main/java/io/reactivesw/customer/server/common/types/TaxRate.java
+++ src/main/java/io/reactivesw/customer/server/common/types/TaxRate.java
@@ -37,4 +37,146 @@ public class TaxRate {
           required = true)
   private List<SubRate> subRates;
 
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets name.
+   *
+   * @param name the name
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Gets amount.
+   *
+   * @return the amount
+   */
+  public float getAmount() {
+    return amount;
+  }
+
+  /**
+   * Sets amount.
+   *
+   * @param amount the amount
+   */
+  public void setAmount(float amount) {
+    this.amount = amount;
+  }
+
+  /**
+   * Gets included in price.
+   *
+   * @return the included in price
+   */
+  public Boolean getIncludedInPrice() {
+    return includedInPrice;
+  }
+
+  /**
+   * Sets included in price.
+   *
+   * @param includedInPrice the included in price
+   */
+  public void setIncludedInPrice(Boolean includedInPrice) {
+    this.includedInPrice = includedInPrice;
+  }
+
+  /**
+   * Gets country.
+   *
+   * @return the country
+   */
+  public String getCountry() {
+    return country;
+  }
+
+  /**
+   * Sets country.
+   *
+   * @param country the country
+   */
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  /**
+   * Gets state.
+   *
+   * @return the state
+   */
+  public String getState() {
+    return state;
+  }
+
+  /**
+   * Sets state.
+   *
+   * @param state the state
+   */
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  /**
+   * Gets sub rates.
+   *
+   * @return the sub rates
+   */
+  public List<SubRate> getSubRates() {
+    return subRates;
+  }
+
+  /**
+   * Sets sub rates.
+   *
+   * @param subRates the sub rates
+   */
+  public void setSubRates(List<SubRate> subRates) {
+    this.subRates = subRates;
+  }
+
+  /**
+   * toString method.
+   * @return String
+   */
+  @Override
+  public String toString() {
+    return "TaxRate{" +
+        "id='" + id + '\'' +
+        ", name='" + name + '\'' +
+        ", amount=" + amount +
+        ", includedInPrice=" + includedInPrice +
+        ", country='" + country + '\'' +
+        ", state='" + state + '\'' +
+        ", subRates=" + subRates +
+        '}';
+  }
 }
