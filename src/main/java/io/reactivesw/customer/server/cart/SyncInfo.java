e2307cd656eba3abe57216fb700bc6a900049908
Davis <davis.dai@go6d.com>
2016-11-17 17:25:34 +0800

    edit SyncInfo

--- src/main/java/io/reactivesw/customer/server/cart/SyncInfo.java
+++ src/main/java/io/reactivesw/customer/server/cart/SyncInfo.java
@@ -1,8 +1,29 @@
 package io.reactivesw.customer.server.cart;
 
+import io.reactivesw.customer.server.common.types.Reference;
+
+import java.time.ZonedDateTime;
+
 /**
  * Created by Davis on 16/11/17.
  */
-//TODO
 public class SyncInfo {
+
+  /**
+   * Reference to a Channel.
+   * Connection to particular synchronization destination.
+   */
+  private Reference channel;
+
+  /**
+   * Can be used to reference an external order instance, file etc.
+   */
+  private String externalId;
+
+
+  /**
+   * The Synced at.
+   */
+  private ZonedDateTime syncedAt;
+
 }
