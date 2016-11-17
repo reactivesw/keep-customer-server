f992a5f108125b002cabd7a5ab37f2b27e9d4a1c
Davis <davis.dai@go6d.com>
2016-11-17 17:29:27 +0800

    edit ReturnInfo

--- src/main/java/io/reactivesw/customer/server/cart/models/ReturnInfo.java
+++ src/main/java/io/reactivesw/customer/server/cart/models/ReturnInfo.java
@@ -1,8 +1,27 @@
 package io.reactivesw.customer.server.cart.models;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 /**
  * Created by Davis on 16/11/17.
  */
-//TODO
 public class ReturnInfo {
+
+  /**
+   * Array of ReturnItem
+   */
+  private List<ReturnItem> items;
+
+  /**
+   * Identifies, which return tracking ID is connected to this particular return.
+   */
+  private String returnTrackingId;
+
+
+  /**
+   * The Return date.
+   */
+  private ZonedDateTime returnDate;
+
 }
