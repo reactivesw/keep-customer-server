42ea4af5985c59bcaea3fc4b55d5feff9e45ed6b
Davis <davis.dai@go6d.com>
2016-11-17 12:26:58 +0800

    edit product model.

--- src/main/java/io/reactivesw/customer/server/catalog/models/AttributeType.java
+++ src/main/java/io/reactivesw/customer/server/catalog/models/AttributeType.java
@@ -4,4 +4,5 @@ package io.reactivesw.customer.server.catalog.models;
  * Created by Davis on 16/11/16.
  */
 public class AttributeType {
+
 }
--- src/main/java/io/reactivesw/customer/server/catalog/models/Product.java
+++ src/main/java/io/reactivesw/customer/server/catalog/models/Product.java
@@ -1,7 +1,7 @@
 package io.reactivesw.customer.server.catalog.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.reactivesw.customer.server.common.types.Reference;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
@@ -11,14 +11,60 @@ import java.util.UUID;
  */
 public class Product {
   /**
-   * The Id.
+   * The unique ID of the product.
    */
   private String id;
+
+  /**
+   * User-specific unique identifier for the product.
+   * Product keys are different from product variant keys.
+   */
+  private String key;
+
+  /**
+   * The current version of the product.
+   */
+  private Integer version;
+
   /**
    * The Create at.
    */
   @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
-  private ZonedDateTime createAt;
+  private ZonedDateTime createdAt;
+
+  /**
+   * last modified time.
+   */
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
+  private ZonedDateTime lastModifiedAt;
+
+  /**
+   * Reference to a ProductType.
+   */
+  private Reference productType;
+
+  /**
+   * The product data in the master catalog.
+   */
+  private ProductCatalogData masterData;
+
+  /**
+   * Reference to a TaxCategory.
+   */
+  private Reference taxCategory;
+
+  /**
+   * Reference to a State.
+   * Optional.
+   */
+  private Reference state;
+
+  /**
+   * Statistics about the review ratings taken into account for this product.
+   * Statistics about the review ratings taken into account for this product.
+   * Optional.
+   */
+  private ReviewRatingStatistics reviewRatingStatistics;
 
   /**
    * Gets id.
@@ -39,25 +85,184 @@ public class Product {
   }
 
   /**
-   * Gets create at.
+   * Gets key.
    *
-   * @return the create at
+   * @return the key
    */
-  public ZonedDateTime getCreateAt() {
-    return createAt;
+  public String getKey() {
+    return key;
   }
 
   /**
-   * Sets create at.
+   * Sets key.
    *
-   * @param createAt the create at
+   * @param key the key
    */
-  public void setCreateAt(ZonedDateTime createAt) {
-    this.createAt = createAt;
+  public void setKey(String key) {
+    this.key = key;
   }
 
-  public Product() {
-    this.id = UUID.randomUUID().toString();
-    this.createAt = ZonedDateTime.now();
+  /**
+   * Gets version.
+   *
+   * @return the version
+   */
+  public Integer getVersion() {
+    return version;
+  }
+
+  /**
+   * Sets version.
+   *
+   * @param version the version
+   */
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  /**
+   * Gets created at.
+   *
+   * @return the created at
+   */
+  public ZonedDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  /**
+   * Sets created at.
+   *
+   * @param createdAt the created at
+   */
+  public void setCreatedAt(ZonedDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  /**
+   * Gets last modified at.
+   *
+   * @return the last modified at
+   */
+  public ZonedDateTime getLastModifiedAt() {
+    return lastModifiedAt;
+  }
+
+  /**
+   * Sets last modified at.
+   *
+   * @param lastModifiedAt the last modified at
+   */
+  public void setLastModifiedAt(ZonedDateTime lastModifiedAt) {
+    this.lastModifiedAt = lastModifiedAt;
+  }
+
+  /**
+   * Gets product type.
+   *
+   * @return the product type
+   */
+  public Reference getProductType() {
+    return productType;
+  }
+
+  /**
+   * Sets product type.
+   *
+   * @param productType the product type
+   */
+  public void setProductType(Reference productType) {
+    this.productType = productType;
+  }
+
+  /**
+   * Gets master data.
+   *
+   * @return the master data
+   */
+  public ProductCatalogData getMasterData() {
+    return masterData;
+  }
+
+  /**
+   * Sets master data.
+   *
+   * @param masterData the master data
+   */
+  public void setMasterData(ProductCatalogData masterData) {
+    this.masterData = masterData;
+  }
+
+  /**
+   * Gets tax category.
+   *
+   * @return the tax category
+   */
+  public Reference getTaxCategory() {
+    return taxCategory;
+  }
+
+  /**
+   * Sets tax category.
+   *
+   * @param taxCategory the tax category
+   */
+  public void setTaxCategory(Reference taxCategory) {
+    this.taxCategory = taxCategory;
+  }
+
+  /**
+   * Gets state.
+   *
+   * @return the state
+   */
+  public Reference getState() {
+    return state;
+  }
+
+  /**
+   * Sets state.
+   *
+   * @param state the state
+   */
+  public void setState(Reference state) {
+    this.state = state;
+  }
+
+  /**
+   * Gets review rating statistics.
+   *
+   * @return the review rating statistics
+   */
+  public ReviewRatingStatistics getReviewRatingStatistics() {
+    return reviewRatingStatistics;
+  }
+
+  /**
+   * Sets review rating statistics.
+   *
+   * @param reviewRatingStatistics the review rating statistics
+   */
+  public void setReviewRatingStatistics(ReviewRatingStatistics reviewRatingStatistics) {
+    this.reviewRatingStatistics = reviewRatingStatistics;
+  }
+
+  /**
+   * toString method.
+   * @return String
+   */
+  @Override
+  public String toString() {
+    return "Product{" +
+        "id='" + id + '\'' +
+        ", key='" + key + '\'' +
+        ", version=" + version +
+        ", createdAt=" + createdAt +
+        ", lastModifiedAt=" + lastModifiedAt +
+        ", productType=" + productType +
+        ", masterData=" + masterData +
+        ", taxCategory=" + taxCategory +
+        ", state=" + state +
+        ", reviewRatingStatistics=" + reviewRatingStatistics +
+        '}';
   }
 }
