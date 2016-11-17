27e2e64f4bc4f0c44bf7919316b94b5c262b91c6
Davis <davis.dai@go6d.com>
2016-11-17 14:40:32 +0800

    add Suggestion and SuggesionResult

---
+++ src/main/java/io/reactivesw/customer/server/catalog/models/SuggestionResult.java
@@ -0,0 +1,42 @@
+package io.reactivesw.customer.server.catalog.models;
+
+import java.util.List;
+
+/**
+ * Created by Davis on 16/11/17.
+ */
+public class SuggestionResult {
+  /**
+   * The Search keywords.
+   */
+  private List<Suggestion> searchKeywords;
+
+  /**
+   * Gets search keywords.
+   *
+   * @return the search keywords
+   */
+  public List<Suggestion> getSearchKeywords() {
+    return searchKeywords;
+  }
+
+  /**
+   * Sets search keywords.
+   *
+   * @param searchKeywords the search keywords
+   */
+  public void setSearchKeywords(List<Suggestion> searchKeywords) {
+    this.searchKeywords = searchKeywords;
+  }
+
+  /**
+   * toString method.
+   * @return String
+   */
+  @Override
+  public String toString() {
+    return "SuggestionResult{" +
+        "searchKeywords=" + searchKeywords +
+        '}';
+  }
+}
