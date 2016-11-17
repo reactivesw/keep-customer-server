3451dc451b8f87434738028958ef652e5a69f13f
umasuo <umasuo@Bruce-Liu.local>
2016-11-17 15:59:30 +0800

    add shipping methods

--- src/main/java/io/reactivesw/customer/server/cart/models/draft/ShippingMethodDraft.java
+++ src/main/java/io/reactivesw/customer/server/cart/models/draft/ShippingMethodDraft.java
@@ -1,6 +1,11 @@
 package io.reactivesw.customer.server.cart.models.draft;
 
+import java.util.List;
+
+import io.reactivesw.customer.server.cart.models.ZoneRate;
+import io.reactivesw.customer.server.common.types.Reference;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * This representation needs to be given with a Create ShippingMethod request.
@@ -9,11 +14,19 @@ import io.swagger.annotations.ApiModel;
 @ApiModel(description = "This representation needs to be given with a Create ShippingMethod request.")
 public class ShippingMethodDraft {
 
+  @ApiModelProperty(required = true)
+  private String name;
 
-//          name - String - Required
-//  description - String - Optional
-//  taxCategory - Reference to a TaxCategory
-//  zoneRates - Array of ZoneRate
-//  isDefault - Boolean
-//  If true the shipping method will be the default one in a project.
+  @ApiModelProperty(required = false)
+  private String description;
+
+  @ApiModelProperty(required = true)
+  private Reference taxCategory;
+
+  @ApiModelProperty(required = true)
+  private List<ZoneRate> zoneRates;
+
+  @ApiModelProperty(value = "If true the shipping method will be the default one in a project.", required = true)
+  private Boolean isDefault;
+
 }
