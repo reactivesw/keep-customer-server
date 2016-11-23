5e0d2c591d4968a404aac3ac1453be471ca0c6e6
Davis <davis.dai@go6d.com>
2016-11-23 15:48:23 +0800

    add ProductDiscounts entities package

--- src/main/java/io/reactivesw/catalog/productdiscounts/models/ProductDiscountValue.java
+++ src/main/java/io/reactivesw/catalog/productdiscounts/models/ProductDiscountValue.java
@@ -1,8 +1,23 @@
 package io.reactivesw.catalog.productdiscounts.models;
 
+import io.reactivesw.common.models.Money;
+
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Davis on 16/11/17.
  */
-//TODO
-public class ProductDiscountValue {
+public interface ProductDiscountValue {
+  static AbsoluteProductDiscountValue ofAbsolute(final List<Money> money) {
+    return AbsoluteProductDiscountValue.of(money);
+  }
+
+  static AbsoluteProductDiscountValue ofAbsolute(final Money money) {
+    return AbsoluteProductDiscountValue.of(Collections.singletonList(money));
+  }
+
+  static RelativeProductDiscountValue ofRelative(final int permyriad) {
+    return RelativeProductDiscountValue.of(permyriad);
+  }
 }
---
+++ src/main/java/io/reactivesw/catalog/productdiscounts/models/RelativeProductDiscountValue.java
@@ -0,0 +1,36 @@
+package io.reactivesw.catalog.productdiscounts.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.reactivesw.common.models.Base;
+
+public final class RelativeProductDiscountValue extends Base implements ProductDiscountValue {
+  private final Integer permyriad;
+
+  @JsonCreator
+  private RelativeProductDiscountValue(final Integer permyriad) {
+    this.permyriad = permyriad;
+  }
+
+  /**
+   * Per ten thousand. The fraction the price is reduced. 1000 will result in a 10% price
+   * reduction.
+   *
+   * @return permyriad
+   */
+  public Integer getPermyriad() {
+    return permyriad;
+  }
+
+  /**
+   * Alias for {@link RelativeProductDiscountValue#getPermyriad()}
+   *
+   * @return permyriad
+   */
+  public Integer getBasisPoint() {
+    return getPermyriad();
+  }
+
+  public static RelativeProductDiscountValue of(final Integer permyriad) {
+    return new RelativeProductDiscountValue(permyriad);
+  }
+}
