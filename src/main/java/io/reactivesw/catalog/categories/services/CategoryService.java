c057247c02eca2a14a975df4ce0d5ab0a112b853
Davis <davis.dai@go6d.com>
2016-11-28 17:07:21 +0800

    add delete method to Category

--- src/main/java/io/reactivesw/catalog/categories/services/CategoryService.java
+++ src/main/java/io/reactivesw/catalog/categories/services/CategoryService.java
@@ -5,6 +5,7 @@ import io.reactivesw.catalog.categories.models.Category;
 import io.reactivesw.catalog.categories.models.mapper.CategoryMapper;
 import io.reactivesw.catalog.categories.repositories.CategoryRepository;
 import io.reactivesw.common.exceptions.NotExistException;
+import io.reactivesw.common.exceptions.ParametersException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,10 +39,32 @@ public class CategoryService {
     LOG.debug("enter getCategoryById, id is {}", id);
     CategoryEntity categoryEntity = categoryRepository.findOne(id);
     if (categoryEntity == null) {
-      LOG.debug("fail to getCategoryById, can not find category by id {}", id);
+      LOG.debug("fail getCategoryById, can not find category by id {}", id);
       throw new NotExistException();
     }
     LOG.debug("end getCategoryById, id is {}, get Category {}", id, categoryEntity.toString());
     return CategoryMapper.entityToCategory(categoryEntity);
+  }
+
+  /**
+   * Delete category by id and version.
+   *
+   * @param id      the id
+   * @param version the version
+   */
+  public void deleteCategory(String id, Integer version) {
+    LOG.debug("enter deleteCategory, id is {}, version is {}", id, version);
+    CategoryEntity entity = categoryRepository.findOne(id);
+    if (entity == null) {
+      LOG.debug("fail deleteCategory, can not find category by id {}", id);
+      throw new NotExistException();
+    }
+    if (!version.equals(entity.getVersion())) {
+      LOG.debug("fail deleteCategory, version not match,delete version is {}, entity version is {}",
+          version, entity.getVersion());
+      throw new ParametersException();
+    }
+    categoryRepository.delete(id);
+    LOG.debug("end deleteCategory, id is {}, version is {}", id, version);
   }
 }
--- src/test/groovy/io/reactivesw/catalog/categories/services/CategoryServiceTest.groovy
+++ src/test/groovy/io/reactivesw/catalog/categories/services/CategoryServiceTest.groovy
@@ -3,6 +3,7 @@ package io.reactivesw.catalog.categories.services
 import io.reactivesw.catalog.categories.entities.CategoryEntity
 import io.reactivesw.catalog.categories.repositories.CategoryRepository
 import io.reactivesw.common.exceptions.NotExistException
+import io.reactivesw.common.exceptions.ParametersException
 import spock.lang.Specification
 
 /**
@@ -13,6 +14,7 @@ class CategoryServiceTest extends Specification {
     CategoryService categoryService = new CategoryService(categoryRepository: categoryRepository);
     def categoryEntity = new CategoryEntity()
     def id = "11111111"
+    def version = 1
 
     def setup() {
         categoryEntity.id = id
@@ -39,5 +41,41 @@ class CategoryServiceTest extends Specification {
 
         then:
         thrown(NotExistException)
+    }
+
+    def "test 3 : delete Category and get null entity"() {
+        given:
+        categoryRepository.findOne(_) >> null
+
+        when:
+        categoryService.deleteCategory(id, version)
+
+        then:
+        thrown(NotExistException)
+    }
+
+    def "test 4 : delete Category and can not match version"() {
+        given:
+        categoryEntity.version = 2
+        categoryRepository.findOne(_) >> categoryEntity
+
+        when:
+        categoryService.deleteCategory(id, version)
+
+        then:
+        thrown(ParametersException)
+    }
+
+    def "test 5 : delete Category"() {
+        given:
+        categoryEntity.version = version
+
+        categoryRepository.findOne(_) >> categoryEntity
+
+        when:
+        categoryService.deleteCategory(id, version)
+
+        then:
+        true
     }
 }
