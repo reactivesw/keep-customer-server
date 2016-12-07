43c20048aca9f3e34a1b476a8076cc3b6a6c1a5a
umasuo <liuquan89@gmail.com>
2016-12-07 16:34:24 +0800

    finish discount code update actions

--- src/main/java/io/reactivesw/order/discountcode/application/model/DiscountCodeDraft.java
+++ src/main/java/io/reactivesw/order/discountcode/application/model/DiscountCodeDraft.java
@@ -1,12 +1,11 @@
 package io.reactivesw.order.discountcode.application.model;
 
-import java.util.List;
-
-import io.reactivesw.order.cart.application.model.CartPredicate;
 import io.reactivesw.common.model.LocalizedString;
 import io.reactivesw.common.model.Reference;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+
+import java.util.List;
 
 /**
  * Created by umasuo on 16/11/17.
@@ -21,89 +20,172 @@ public class DiscountCodeDraft {
   private LocalizedString description;
 
   @ApiModelProperty(value = "Unique identifier of this discount code. " +
-          "This value is added to the cart to enable the related cart discounts in the cart.",
-          required = true)
+      "This value is added to the cart to enable the related cart discounts in the cart.",
+      required = true)
   private String code;
 
-  @ApiModelProperty(value = "The referenced matching cart discounts can be applied to the cart once the DiscountCode is added.",
-          required = true)
+  @ApiModelProperty(value = "The referenced matching cart discounts can be applied to the cart " +
+      "once the DiscountCode is added.",
+      required = true)
   private List<Reference> cartDiscounts;
 
-  @ApiModelProperty(value = "The discount code can only be applied to cart that match this predicate.",
-          required = false)
-  private CartPredicate cartPredicate;
+  @ApiModelProperty(value = "The discount code can only be applied to cart that match this " +
+      "predicate.",
+      required = false)
+  private String cartPredicate;
 
   @ApiModelProperty(required = true)
   private Boolean isActive;
 
   @ApiModelProperty(value = "The discount code can only be applied maxApplications times.",
-          required = false)
+      required = false)
   private Integer maxApplications;
 
-  @ApiModelProperty(value = "The discount code can only be applied maxApplicationsPerCustomer times per customer.",
-          required = true)
+  @ApiModelProperty(value = "The discount code can only be applied maxApplicationsPerCustomer " +
+      "times per customer.",
+      required = true)
   private Integer maxApplicationsPerCustomer;
 
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
   public LocalizedString getName() {
     return name;
   }
 
+  /**
+   * Sets name.
+   *
+   * @param name the name
+   */
   public void setName(LocalizedString name) {
     this.name = name;
   }
 
+  /**
+   * Gets description.
+   *
+   * @return the description
+   */
   public LocalizedString getDescription() {
     return description;
   }
 
+  /**
+   * Sets description.
+   *
+   * @param description the description
+   */
   public void setDescription(LocalizedString description) {
     this.description = description;
   }
 
+  /**
+   * Gets code.
+   *
+   * @return the code
+   */
   public String getCode() {
     return code;
   }
 
+  /**
+   * Sets code.
+   *
+   * @param code the code
+   */
   public void setCode(String code) {
     this.code = code;
   }
 
+  /**
+   * Gets cart discounts.
+   *
+   * @return the cart discounts
+   */
   public List<Reference> getCartDiscounts() {
     return cartDiscounts;
   }
 
+  /**
+   * Sets cart discounts.
+   *
+   * @param cartDiscounts the cart discounts
+   */
   public void setCartDiscounts(List<Reference> cartDiscounts) {
     this.cartDiscounts = cartDiscounts;
   }
 
-  public CartPredicate getCartPredicate() {
+  /**
+   * Gets cart predicate.
+   *
+   * @return the cart predicate
+   */
+  public String getCartPredicate() {
     return cartPredicate;
   }
 
-  public void setCartPredicate(CartPredicate cartPredicate) {
+  /**
+   * Sets cart predicate.
+   *
+   * @param cartPredicate the cart predicate
+   */
+  public void setCartPredicate(String cartPredicate) {
     this.cartPredicate = cartPredicate;
   }
 
+  /**
+   * Gets active.
+   *
+   * @return the active
+   */
   public Boolean getActive() {
     return isActive;
   }
 
+  /**
+   * Sets active.
+   *
+   * @param active the active
+   */
   public void setActive(Boolean active) {
     isActive = active;
   }
 
+  /**
+   * Gets max applications.
+   *
+   * @return the max applications
+   */
   public Integer getMaxApplications() {
     return maxApplications;
   }
 
+  /**
+   * Sets max applications.
+   *
+   * @param maxApplications the max applications
+   */
   public void setMaxApplications(Integer maxApplications) {
     this.maxApplications = maxApplications;
   }
 
+  /**
+   * Gets max applications per customer.
+   *
+   * @return the max applications per customer
+   */
   public Integer getMaxApplicationsPerCustomer() {
     return maxApplicationsPerCustomer;
   }
 
+  /**
+   * Sets max applications per customer.
+   *
+   * @param maxApplicationsPerCustomer the max applications per customer
+   */
   public void setMaxApplicationsPerCustomer(Integer maxApplicationsPerCustomer) {
     this.maxApplicationsPerCustomer = maxApplicationsPerCustomer;
   }
--- src/main/java/io/reactivesw/order/discountcode/application/model/mapper/CartPredicateMapper.java
+++ src/main/java/io/reactivesw/order/discountcode/application/model/mapper/CartPredicateMapper.java
@@ -1,6 +1,7 @@
 package io.reactivesw.order.discountcode.application.model.mapper;
 
 import io.reactivesw.common.model.UpdateAction;
+import io.reactivesw.order.discountcode.application.model.action.SetCartPredicate;
 import io.reactivesw.order.discountcode.domain.entity.DiscountCodeEntity;
 
 /**
@@ -8,8 +9,16 @@ import io.reactivesw.order.discountcode.domain.entity.DiscountCodeEntity;
  */
 public class CartPredicateMapper implements DiscountCodeUpdateMapper<DiscountCodeEntity> {
 
+  /**
+   * set cart predicate.
+   *
+   * @param entity DiscountCodeEntity
+   * @param action UpdateAction
+   */
   @Override
   public void setAction(DiscountCodeEntity entity, UpdateAction action) {
-
+    SetCartPredicate predicate = (SetCartPredicate) action;
+    //TODO how to receive the cart predicate
+    entity.setCartPredicate(predicate.getCartPredicate());
   }
 }
