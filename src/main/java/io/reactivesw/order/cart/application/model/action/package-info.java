e40f939f1f98756077a37b95d068dd41fe319c35
Davis <davis.dai@go6d.com>
2016-12-07 18:06:31 +0800

    remove order package-info.java

--- src/main/java/io/reactivesw/order/cart/application/model/action/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by umasuo on 16/12/1.
- */
-package io.reactivesw.order.cart.application.model.action;
--- src/main/java/io/reactivesw/order/cart/domain/entity/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by umasuo on 16/11/25.
- */
-package io.reactivesw.order.cart.domain.entity;
