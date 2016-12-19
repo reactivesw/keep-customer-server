4c578ab6f5339caa5b45c75fdcdde2fbdb838dc1
umasuo <liuquan89@gmail.com>
2016-12-19 20:04:40 +0800

    cart update: remove payment service

--- src/main/java/io/reactivesw/order/cart/application/model/action/RemovePayment.java
+++ src/main/java/io/reactivesw/order/cart/application/model/action/RemovePayment.java
@@ -1,16 +1,17 @@
 package io.reactivesw.order.cart.application.model.action;
 
 import io.reactivesw.common.model.Reference;
+import io.reactivesw.common.model.UpdateAction;
 import lombok.Data;
 
 /**
  * Created by umasuo on 16/12/15.
  */
 @Data
-public class RemovePayment {
+public class RemovePayment implements UpdateAction {
 
   /**
    * reference to an payment.
    */
-  private Reference payment;
+  private String payment;
 }
