2d2f4a77f98c95bd8d3e95a1016ec5ac88ee35ab
umasuo <liuquan89@gmail.com>
2016-12-19 20:14:35 +0800

    cat update: set tax mode service

---
+++ src/test/groovy/io/reactivesw/order/cart/domain/service/update/SetTaxModeServiceTest.groovy
@@ -0,0 +1,41 @@
+package io.reactivesw.order.cart.domain.service.update
+
+import io.reactivesw.order.cart.application.model.action.RemovePayment
+import io.reactivesw.order.cart.application.model.action.SetTaxMode
+import io.reactivesw.order.cart.domain.entity.CartEntity
+import io.reactivesw.order.cart.infrastructure.enums.TaxMode
+import spock.lang.Specification
+
+/**
+ * Created by umasuo on 16/12/19.
+ */
+class SetTaxModeServiceTest extends Specification {
+
+    SetTaxModeService service
+
+    def cartId = "tmpCartId"
+
+    def version = 1
+
+    CartEntity cartEntity
+
+    SetTaxMode setTaxMode
+
+    def setup() {
+
+        cartEntity = new CartEntity(id: cartId, version: version,)
+
+        setTaxMode = new SetTaxMode(taxMode: TaxMode.Platform)
+
+        service = new SetTaxModeService()
+    }
+
+    def "Test 1.1: set tax mode"() {
+
+        when:
+        service.handle(cartEntity, setTaxMode)
+        then:
+        noExceptionThrown()
+
+    }
+}
