998caf7d7d156eb6e76d340a8e7668ce41fa64f2
Davis <davis.dai@go6d.com>
2016-12-21 17:40:09 +0800

    add AddQuantity to inventoryentry

--- src/main/java/io/reactivesw/catalog/inventory/application/model/mapper/InventoryEntryUpdateMapper.java
+++ src/main/java/io/reactivesw/catalog/inventory/application/model/mapper/InventoryEntryUpdateMapper.java
@@ -3,7 +3,10 @@ package io.reactivesw.catalog.inventory.application.model.mapper;
 import com.google.common.collect.ImmutableMap;
 
 import io.reactivesw.catalog.category.application.model.mapper.CategoryUpdateMapper;
+import io.reactivesw.catalog.inventory.application.model.action.AddQuantityAction;
 import io.reactivesw.common.model.Update;
+
+import org.apache.commons.collections4.Put;
 
 import java.util.Map;
 
@@ -12,7 +15,8 @@ import java.util.Map;
  */
 public interface InventoryEntryUpdateMapper<E> extends Update<E> {
   Map<Class<?>, InventoryEntryUpdateMapper> updateMappers = ImmutableMap.<Class<?>,
-      InventoryEntryUpdateMapper>builder().build();
+      InventoryEntryUpdateMapper>builder()
+      .put(AddQuantityAction.class, new AddQuantityMapper()).build();
 
   /**
    * @param clazz
