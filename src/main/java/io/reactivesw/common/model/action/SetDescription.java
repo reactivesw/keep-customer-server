6d9b5d5d639e397658f7eb93196ce323e36cde78
Davis <davis.dai@go6d.com>
2017-01-05 15:06:30 +0800

    fix product type bug : set description

--- src/main/java/io/reactivesw/common/model/action/SetDescription.java
+++ src/main/java/io/reactivesw/common/model/action/SetDescription.java
@@ -1,22 +1,30 @@
 package io.reactivesw.common.model.action;
 
+import io.reactivesw.catalog.producttype.infrastructure.util.ProductTypeActionUtils;
 import io.reactivesw.common.model.UpdateAction;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Created by Davis on 16/12/8.
  */
-@Data
+@Getter
+@Setter
 public class SetDescription implements UpdateAction {
   /**
    * The Description.
    */
-  protected String description;
-
-  protected String name;
+  @NotNull
+  @Size(min = 1)
+  private String description;
 
   @Override
   public String getActionName() {
-    return null;
+    return ProductTypeActionUtils.SET_DESCRIPTION;
   }
 }
