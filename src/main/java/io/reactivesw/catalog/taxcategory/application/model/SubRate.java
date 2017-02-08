60793bb42670c108d0daf53fc9eab0c86e58dbf7
Davis <davis.dai@go6d.com>
2017-02-08 09:35:29 +0800

    edit order SubRateMapper

--- src/main/java/io/reactivesw/catalog/taxcategory/application/model/SubRate.java
+++ src/main/java/io/reactivesw/catalog/taxcategory/application/model/SubRate.java
@@ -3,9 +3,14 @@ package io.reactivesw.catalog.taxcategory.application.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by umasuo on 16/11/17.
  */
+@Getter
+@Setter
 @ApiModel(description = "A SubRateEntity is used to calculate the taxPortions field in a cart or order. It is useful if the total tax of a country is a combination of multiple taxes (e.g. state and local taxes).")
 public class SubRate {
 
@@ -15,20 +20,4 @@ public class SubRate {
 
   @ApiModelProperty(value = "Number Percentage in the range of [0..1].", required = true)
   private Float amount;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Float getAmount() {
-    return amount;
-  }
-
-  public void setAmount(Float amount) {
-    this.amount = amount;
-  }
 }
--- src/main/java/io/reactivesw/order/order/application/model/mapper/SubRateMapper.java
+++ src/main/java/io/reactivesw/order/order/application/model/mapper/SubRateMapper.java
@@ -1,5 +1,6 @@
 package io.reactivesw.order.order.application.model.mapper;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import io.reactivesw.catalog.taxcategory.application.model.SubRate;
@@ -56,11 +57,37 @@ public final class SubRateMapper {
   /**
    * Entity to model list.
    *
-   * @param models the models
+   * @param entities the models
    * @return the list
    */
-  public static List<SubRate> entityToModel(Set<SubRateValue> models) {
-    // TODO: 17/2/8
-    return null;
+  public static List<SubRate> entityToModel(Set<SubRateValue> entities) {
+    List<SubRate> models = Lists.newArrayList();
+
+    if (entities != null) {
+      models = entities.parallelStream().map(
+          entity -> {
+            return entityToModel(entity);
+          }
+      ).collect(Collectors.toList());
+    }
+
+    return models;
+  }
+
+  /**
+   * Entity to model sub rate.
+   *
+   * @param entity the entity
+   * @return the sub rate
+   */
+  public static SubRate entityToModel(SubRateValue entity) {
+    SubRate model = new SubRate();
+
+    if (entity != null) {
+      model.setName(entity.getName());
+      model.setAmount(entity.getAmount());
+    }
+
+    return model;
   }
 }
